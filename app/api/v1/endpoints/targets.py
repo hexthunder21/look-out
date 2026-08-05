79d6814d817651cdf461b360e3f45283c48b6cf6
hexThunder <nazar200792@gmail.com>
2026-08-06 00:50:36 +0300

    /del-target handle was created

--- app/api/v1/endpoints/targets.py
+++ app/api/v1/endpoints/targets.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import APIRouter, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import select, delete, update
 from starlette import status
 from app.schemas.targets import CreateTarget, TargetResponse, TargetWithMessageResponse, TargetType
 from app.api.deps import TargetParamsDep, CurrentUserDep, DBSessionDep
@@ -46,10 +46,8 @@ async def get_target(
         target: str = Query(..., description="Email, phone or username to search for")
         ):
     data_type, credential = await parse_target_credentials(target)
-    print(data_type, credential)
-
     field = TARGET_MAP.get(data_type)
-    print(field)
+
     if field is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid target type or format")
 
@@ -64,3 +62,26 @@ async def get_target(
             )
 
     return target_obj
+
+
+@router.delete("/del-target", response_model=TargetWithMessageResponse)
+async def delete_target(
+        current_user: CurrentUserDep,
+        db: DBSessionDep,
+        target: str = Query(..., description="Email, phone or username to search for")
+        ):
+    data_type, credential = await parse_target_credentials(target)
+    field = TARGET_MAP.get(data_type)
+
+    if field is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid target type or format")
+
+    query = delete(Target).where(Target.user_id == current_user.id, field == credential)
+    result = await db.execute(query)
+    await db.commit()
+
+    if result.rowcount == 0:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Target not found")
+
+    return {"message": "Target deleted successfully"}
+
